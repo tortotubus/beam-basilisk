2f87be7fde609fd446c0e0d24b2ddd5d3527437f
Stephane Popinet <popinet@basilisk.fr>
2022-03-09 18:05:20 +0000

    Solver initialization and termination

--- src/ast/init_solver.h
+++ src/ast/init_solver.h
@@ -4,8 +4,6 @@ static void _init_solver (void)
 {
   void init_solver();
   init_solver();
-  Events = (Event *) pmalloc (sizeof (Event), __func__, __FILE__, __LINE__);
-  Events[0].last = 1;
   {
 
     /**
