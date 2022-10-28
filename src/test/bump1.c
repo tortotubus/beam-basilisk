3e65d9b51b8ef8c42e7fab967c0e436b45832355
Stephane Popinet <popinet@basilisk.fr>
2022-10-28 09:03:04 +0000

    Too many flux variables in 1D

--- src/test/bump1.c
+++ src/test/bump1.c
@@ -13,7 +13,6 @@ void flux (const double * s, double * f, double e[2])
   double h = s[0], q = s[1], u = q/h;
   f[0] = q;
   f[1] = q*u + G*h*h/2.;
-  f[2] = s[2]*u;
   // min/max eigenvalues
   double c = sqrt(G*h);
   e[0] = u - c; // min
