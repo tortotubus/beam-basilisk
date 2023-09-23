b9ae76a27fe22af08ce0ebd30ac0f997d53c2874
Stephane Popinet <popinet@basilisk.fr>
2023-09-23 08:38:53 +0000

    Inline plot

--- src/test/halo.c
+++ src/test/halo.c
@@ -1,4 +1,19 @@
-/* definition of halo cells after coarsening */
+/**
+# Definition of halo cells after coarsening 
+
+~~~gnuplot
+set size ratio -1
+unset grid
+unset xtics
+unset ytics
+unset border
+set key outside
+plot			  \
+     'out' w l t "" lc 0, \
+     '< grep halo log | awk "(\$3==4){print \$0}"' pt 7 lc 1 t 'halo 4', \
+     '< grep halo log | awk "(\$3==3){print \$0}"' pt 7 lc 2 t 'halo 3'
+~~~
+*/
 
 scalar h[];
 
@@ -12,6 +27,9 @@ int main (int argc, char ** argv)
     h[] = exp(-(x*x + y*y)/(R0*R0));
   
   /* initial coarsening */
+
+  double toto;
+  toto;
   
   while (adapt_wavelet ({h}, (double []){1e-2}, 5).nc);
 
