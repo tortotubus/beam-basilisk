253dcc410b2b1a677c74376247f4780a53e1031a
Stephane Popinet <popinet@basilisk.fr>
2023-12-09 12:12:48 +0000

    Cleanup of the reflectionperfect.c test case

--- src/test/reflectionperfect.c
+++ src/test/reflectionperfect.c
@@ -1,74 +1,92 @@
 /**
 # Zero reflection of a wave propagating across an interface between two fluids with impedance matching
 
-In this test proposed [Denner et al (JCP, 2018)](https://www.sciencedirect.com/science/article/pii/S0021999118302535)
-a linear wave propagating in an ideal gas is completely transmitted to another ideal gas with the same acoustic impedance
-*/
+In this test proposed by [Denner et al, 2018](#denner2018) a linear wave
+propagating in an ideal gas is completely transmitted to another ideal
+gas with the same acoustic impedance. */
 
 #include "two-phase-compressible.h"
 
-/** Parameters of the problem */
+/** 
+Parameters of the problem. */
+
 double tend = 0.6;
 double cflac = 0.1;
-double uper = 0.0001;
+double uper = 1e-4;
 double freq = 15.;
 double p0, rho20;
 
-event stability (i++) {
-  double dt = 100.;
-  foreach ()
-    dt = min(dt,Delta);
-  dtmax = dt*cflac;
-  DT = dt*cflac;
+/**
+Fixme: `cflac` should be a parameter of two-phase-compressible.h. */
+
+event stability (i++)
+{
+  double Delta_min = HUGE;
+  foreach (reduction(min:Delta_min))
+    if (Delta < Delta_min)
+      Delta_min = Delta;
+  dtmax = Delta_min*cflac;
+  DT = dtmax;
 }
 
-
 int main()
-{  
+{
+
+  /** 
+  The EOS for an adiabatic perfect gas is defined by its polytropic
+  coefficient $\Gamma = \gamma$. */
   
-  /** The EOS for an adiabatic perfect gas is defined by its polytropic coefficient $\Gamma = \gamma$ */
   gamma1 = 9.872;
   gamma2 = 2.468;
-  rho20  = 1.0/0.25;
+  rho20  = 1./0.25;
 
   p0 = 1./gamma1;
   
-  /** We use an upwind method for the tracer advection associated to the VOF tracer f*/
+  /** 
+  We use an upwind method for the tracer advection associated to the
+  VOF tracer f. */
+  
   f.gradient = zero; 
 
-  L0 = 1. [0]; // fixme: this should work with dimensions
+  /**
+  We make everything dimensionless but this should be improved. */
+  
+  L0 = 1. [0];
   DT = HUGE [0];
   
   N = 512;
   run();
-
 }
 
 event init (i = 0)
-{   
-  mask( y > 0.01 ? top : none); 
+{
+
+  /**
+  We cannot use multigrid1D because of VOF, so we use masking
+  instead. */
+  
+  mask (y > 0.01 ? top : none); 
 
   foreach() {
-    double perturb = uper*exp(-pow((x-0.3)*freq,2));
-    f[] = ( x < 0.5);
+    double perturb = uper*exp(- sq ((x - 0.3)*freq));
+    f[] = (x < 0.5);
     p[] = p0 + perturb;
     frho1[] = f[]*(1. + perturb);
     frho2[] = (1. - f[])*rho20;
     q.x[] = (frho1[] + frho2[])*perturb;
     q.y[] = 0.;
-    fE1[] = f[]*p[]/(gamma1 - 1.) + 0.5*pow(q.x[]/(frho1[] + frho2[]),2)*frho1[];
-    fE2[] = (1.-f[])*p[]/(gamma2 - 1.) + 0.5*pow(q.x[]/(frho1[] + frho2[]),2)*frho2[];
+    fE1[] = f[]*p[]/(gamma1 - 1.) + 0.5*sq(q.x[]/(frho1[] + frho2[]))*frho1[];
+    fE2[] = (1. - f[])*p[]/(gamma2 - 1.) + 0.5*sq(q.x[]/(frho1[] + frho2[]))*frho2[];
   }
-  boundary ((scalar *){q, frho1, frho2,p, fE1, fE2});
 }
 
-event endprint ( t = tend) {
-  
+event endprint (t = tend)
+{  
   scalar perr[];
   foreach () {
-    perr[] = fabs((p[] - p0)/uper - exp(-sq((x-0.6)*freq*sqrt(gamma1*rho20/gamma2))));
+    perr[] = fabs((p[] - p0)/uper - exp(- sq((x - 0.6)*freq*sqrt(gamma1*rho20/gamma2))));
     if (y < Delta)
-      fprintf (stderr, "%g %g %g \n", t, x, (p[]-p0));
+      fprintf (stderr, "%g %g %g \n", t, x, p[] - p0);
   }
   
   assert (statsf(perr).sum < 3.e-5);
@@ -82,6 +100,25 @@ set samples 1000
 set arrow from 0.5,0 to 0.5,1 nohead lc 0
 set label 'fluid 1' at 0.4,0.8
 set label 'fluid 2' at 0.51,0.8
-p "log" u 2:($3/0.0001) t 'tend' w l lc 1, exp(-((x-0.3)*15)**2) t 't=0' w l, exp(-((x-0.6)*15*sqrt(9.872/2.468/0.25))**2) t 'tend (theory)' w l
+p "log" u 2:($3/0.0001) t 'tend' w l lc 1,				        \
+   exp(-((x - 0.3)*15)**2) t 't = 0' w l,					\
+   exp(-((x - 0.6)*15*sqrt(9.872/2.468/0.25))**2) t 'tend (theory)' w l
+~~~
+
+## References
+
+~~~bib
+@article{denner2018,
+title = {Pressure-based algorithm for compressible interfacial flows
+with acoustically-conservative interface discretisation},
+journal = {Journal of Computational Physics},
+volume = {367},
+pages = {192-234},
+year = {2018},
+issn = {0021-9991},
+doi = {https://doi.org/10.1016/j.jcp.2018.04.028},
+url = {https://www.sciencedirect.com/science/article/pii/S0021999118302535},
+author = {Fabian Denner and Cheng-Nian Xiao and Berend G.M. {van Wachem}}
+}
 ~~~
 */
