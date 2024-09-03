59167962654999bfa7bea4b74df0415e376fecb6
Stephane Popinet <popinet@basilisk.fr>
2024-09-03 07:48:29 +0000

    HAL blocks wget

--- src/darcsit/gethal.sh
+++ src/darcsit/gethal.sh
@@ -2,7 +2,8 @@
 
 cache="$HOME/.halcache"
 if ! found=`grep "$1" $cache 2> /dev/null`; then
-    found=`wget -q -O - https://hal.archives-ouvertes.fr/$1/bibtex`
+    echo "looking for $1 on HAL..." > /dev/stderr
+    found=`wget --user-agent="" -q -O - https://hal.archives-ouvertes.fr/$1/bibtex`
     if test -z "$found"; then
 	found="@misc{error, title={$1 not found or server error}}"
 	echo $found > /dev/stderr
