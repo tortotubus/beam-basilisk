34a164453f630325a54f36da2b80feb2ed771c48
Stephane Popinet <popinet@basilisk.fr>
2024-11-23 13:58:10 +0000

    Gulf Stream documentation

--- src/test/boundaries2.c
+++ src/test/boundaries2.c
@@ -1,3 +1,6 @@
+/**
+# Boundary conditions with 2 ghost layers */
+
 #define BGHOSTS 2
 #include "grid/multigrid.h"
 
@@ -25,13 +28,13 @@ int main()
   foreach_face()
     f.x[] = 1.;
 
-  foreach()
+  foreach (serial)
     foreach_neighbor()
       fprintf (stderr, "%g %g %g %g %g\n", x, y, s[], v.x[], v.y[]);
-  foreach_face(x)
+  foreach_face (x, serial)
     for (int i = -2; i <= 2; i++)
       fprintf (stderr, "%g %g %g\n", x, y + i*Delta, f.x[0,i]);
-  foreach_face(y)
+  foreach_face (y, serial)
     for (int i = -2; i <= 2; i++)
       fprintf (stderr, "%g %g %g\n", x + i*Delta, y, f.y[i]);
 }
