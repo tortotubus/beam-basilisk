c0a7d1cdb6bcb88407ed96d7a7b3263617042882
Stephane Popinet <popinet@basilisk.fr>
2025-03-07 19:16:25 +0000

    GPUs can use multiple SSBOs to access large video memories

--- src/grid/gpu/cartesian.h
+++ src/grid/gpu/cartesian.h
@@ -6,14 +6,14 @@
 #define _GPU 1
 #define GRIDNAME "Cartesian (GPU)"
 #define GRIDPARENT Cartesian
-#define field_size() sq(N + 2)
+#define field_size() sq((size_t)N + 2)
 #define grid_data() (cartesian->d)
 #define field_offset(s,level) ((s).i*field_size())
 #define depth() 0
-#define GPU_CODE()							                   \
-  "#define valt(s,k,l,m) _data[_index(s,m)*field_size() + (point.i + (k))*(N + 2) +"       \
-  " point.j + (l)]\n"							                   \
-  "#define val_red_(s) _data[(s).i*field_size() + (point.i - 1)*NY + point.j - 1]\n"
+#define GPU_CODE()							\
+  "#define valt(s,k,l,m) "						\
+  "_data_val(_index(s,m), (point.i + (k))*(N + 2) + point.j + (l))\n" \
+  "#define val_red_(s) _data_val((s).i, (point.i - 1)*NY + point.j - 1)\n"
 
 static bool _gpu_done_ = false;
 
