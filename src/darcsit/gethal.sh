444b6a11ab3f528feca00f8464529fc87c83d445
Stephane Popinet <popinet@basilisk.fr>
2025-06-12 14:39:39 +0000

    'gethal' uses api.archives-ouvertes.fr

--- src/darcsit/gethal.sh
+++ src/darcsit/gethal.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cache="$HOME/.halcache"
-if ! found=`grep "$1" $cache 2> /dev/null`; then
+# cache="$HOME/.halcache"
+# if ! found=`grep "$1" $cache 2> /dev/null`; then
     echo "looking for $1 on HAL..." > /dev/stderr
-    found=`wget --user-agent="" -q -O - https://hal.archives-ouvertes.fr/$1/bibtex`
+    found=`wget --user-agent="" -q -O - 'https://api.archives-ouvertes.fr/search/?q=halId_s:'$1'&wt=bibtex&rows=1'`
     if test -z "$found"; then
 	found="@misc{error, title={$1 not found or server error}}"
 	echo $found > /dev/stderr
-    else
-	echo $found >> $cache
+#    else
+#	echo $found >> $cache
     fi
-fi
+# fi
 echo $found
