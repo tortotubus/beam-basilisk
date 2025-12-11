a314cea690fa1addc725f5002c3c9b86d83fcde1
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2025-12-11 20:28:30 +0000

    Formatting test for face fraction on trees

--- src/test/facefractiontree.c
+++ src/test/facefractiontree.c
@@ -1,37 +1,37 @@
 /**
-# Test for reconstructed face fractions. */
+# Face fraction on trees
 
-//#include "grid/cartesian.h"
+This test verifies that the face fraction calculation works well also on tree
+grids. */
+
 #include "fractions.h"
-#include "src/fracface.h"
+#include "fracface.h"
 
-int main()
-{
+int main (void) {
   origin (-0.5, -0.5);
-  init_grid (4);
+  init_grid (1 << 2);
 #if TREE
   refine (level == 2 && x < 0.25);
 #endif
   FILE * fp = fopen ("cells", "w");
   output_cells (fp);
   fclose (fp);
- 
+
   vertex scalar phi[];
   foreach_vertex()
     phi[] = 0.1 - sq(x) - sq(y);
-  
+
   scalar c[];
 #if TREE
   c.prolongation = fraction_refine;
 #endif
+
   face vector sf[];
   fractions (phi, c);
   face_fraction (c, sf);
-  fp = fopen ("log", "w");
   foreach_face()
-    fprintf(fp,"%g %g %g \n", x, y, sf.x[]);
-  fclose (fp);
-  fp = fopen ("facet", "w");
+    fprintf (stderr, "%g %g %.3g\n", x, y, sf.x[]);
+  fp = fopen ("facets", "w");
   output_facets (c, fp);
   fclose (fp);
 }
@@ -42,6 +42,6 @@ set size ratio -1
 unset key 
 unset border
 unset tics
-plot 'cells' w l, 'facet' w l, 'log' u 1:2:3 with labels
+plot 'cells' w l, 'facets' w l, 'log' u 1:2:3 with labels
 ~~~
 */
