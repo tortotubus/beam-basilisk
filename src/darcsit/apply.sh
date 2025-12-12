83ae166056f3c3c2be5d1307ac5dc52d85feaafd
Stephane Popinet <popinet@basilisk.fr>
2025-12-12 15:52:34 +0000

    Apply posthook was not working for multiple patches

--- src/darcsit/apply.sh
+++ src/darcsit/apply.sh
@@ -1,7 +1,8 @@
 # If any applied patches adds or removes files, the Makefile.tests in
 # the corresponding directories need to be re-generated
-for date in `echo $DARCS_PATCHES_XML | grep --only-matching "date='[0-9]*'" | \
-    sed "s/date='\([0-9]*\)'/\1/g"`; do
+dates=`echo $DARCS_PATCHES_XML | sed 's|</patch>|\n|g' | grep --only-matching "date='[0-9]*'" | \
+    sed "s/date='\([0-9]*\)'/\1/g"`
+for date in $dates; do
     for f in `darcs changes -v --match "date $date" |        \
     	grep -E '^    (add|rm)file ' | \
     	sed -E 's/^    (add|rm)file //g'`; do
